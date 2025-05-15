ecc2065abfd586b5da20ac9c2eb6c1409f2c2ef2
Dafrijder <nielsdegast@outlook.com>
2025-05-15 11:35:15 +0200

    eis 1 van eisen paket 1

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -17,11 +17,15 @@ namespace PRA_B4_FOTOKIOSK.controller
 
         // De lijst met fotos die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
+
+        public string date;
         
         
         // Start methode die wordt aangeroepen wanneer de foto pagina opent.
         public void Start()
         {
+            var now = DateTime.Now;
+            int day = (int)now.DayOfWeek;
 
             // Initializeer de lijst met fotos
             // WAARSCHUWING. ZONDER FILTER LAADT DIT ALLES!
@@ -32,13 +36,16 @@ namespace PRA_B4_FOTOKIOSK.controller
                  * dir string is de map waar de fotos in staan. Bijvoorbeeld:
                  * \fotos\0_Zondag
                  */
-                foreach (string file in Directory.GetFiles(dir))
+                if (dir.Contains(day.ToString()))
                 {
-                    /**
-                     * file string is de file van de foto. Bijvoorbeeld:
-                     * \fotos\0_Zondag\10_05_30_id8824.jpg
-                     */
-                    PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                    foreach (string file in Directory.GetFiles(dir))
+                    {
+                        /**
+                         * file string is de file van de foto. Bijvoorbeeld:
+                         * \fotos\0_Zondag\10_05_30_id8824.jpg
+                         */
+                        PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                    }
                 }
             }
 
